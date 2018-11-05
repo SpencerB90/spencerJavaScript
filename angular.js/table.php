f2bed8a90bf040f40c7798da59f47b1047490bb3
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-05 08:50:11 -0500

    test

--- angular.js/table.php
+++ angular.js/table.php
@@ -4,12 +4,12 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $conn = new mysqli("localhost", "spencer", "southhills#", "spencer");
 
-$result = $conn->query("SELECT userid, first_name, last_name, city FROM angular_people");
+$result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
-	$outp .= '{"First Name":"'   . $rs["first_name"]    	. '",';
+	$outp .= '{"First Name":"' . $rs["first_name"] . '",';
 	$outp .= '"Last Name":"'. $rs["last_name"] 	. '"';
   $outp .= '"City":"'. $rs["city"] 	. '"}';
 }
