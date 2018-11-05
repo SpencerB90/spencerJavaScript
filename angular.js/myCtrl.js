fb7d63d3eb71b3a0e77c6a717e42ea059bacd69b
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-05 08:17:19 -0500

    test

--- angular.js/myCtrl.js
+++ angular.js/myCtrl.js
@@ -4,5 +4,11 @@ app.controller("myCtrl", function($scope, $http) {
 
   $http.get("json_sample.html").then(function (response) {
     $scope.myData = response.data.records;
+
   });
+
+  $http.get("table.php").then(function (response) {
+    $scope.myDatas = response.data.records;
+
+    });
 });
