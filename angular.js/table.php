a964080118ab0e67d04211de2b2327823a3416b9
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-05 11:05:35 -0500

    test

--- angular.js/table.php
+++ angular.js/table.php
@@ -9,8 +9,8 @@ $result = $conn->query("SELECT first_name, last_name, city FROM angular_people")
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
-	$outp .= '{"First Name":"' . $rs["first_name"] . '",';
-	$outp .= '"Last Name":"'. $rs["last_name"] 	. '"';
+	$outp .= '{"FirstName":"' . $rs["first_name"] . '",';
+	$outp .= '"LastName":"'. $rs["last_name"] 	. '"';
   $outp .= '"City":"'. $rs["city"] 	. '"}';
 }
 $outp ='{"records":['.$outp.']}';
