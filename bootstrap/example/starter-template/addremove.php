68d9a201856399da7e7c0dfd31edb5f8f7f12cfd
SpencerB90 <42805967+SpencerB90@users.noreply.github.com>
2018-11-20 15:14:07 -0500

    test

--- bootstrap/example/starter-template/addremove.php
+++ bootstrap/example/starter-template/addremove.php
@@ -1,16 +1,16 @@
 
 <?php
 
+//includes the database connection
 include('table.php');
 
-
+//grabs the post data
 $_POST = json_decode(file_get_contents('php://input'), true);
 
+//grabs the post data for button selection
 $request_type = $_POST['request_type'];
 
-
 // Get all records
-//if($request_type == 1){
  $sel = mysqli_query($conn,"SELECT id, task, complete FROM toDo");
  $data = array();
 
@@ -18,17 +18,11 @@ $request_type = $_POST['request_type'];
   $data[] = array("id"=>$row['id'],"task"=>$row['task'],"complete"=>$row['complete']);
  }
 
-//}
-
 // Insert task
 if($request_type == 2){
  $task = $_POST['task'];
 
  mysqli_query($conn,"insert into toDo (task) values ('$task')");
- //$lastinsert_id = mysqli_insert_id($conn);
-
- //$return_arr[] = array("id"=>$lastinsert_id,"task"=>$task);
-// echo json_encode($return_arr);
 
 while ($row = mysqli_fetch_array($sel)) {
  $data[] = array("id"=>$row['id'],"task"=>$row['task'],"complete"=>$row['complete']);
@@ -52,10 +46,6 @@ if($request_type == 4){
  $Uid = $_POST['id'];
 
  mysqli_query($conn,"update toDo set complete = 1 where id = ('$Uid')");
- // $lastinsert_id = mysqli_insert_id($conn);
- //
- // $return_arr[] = array("id"=>$lastinsert_id,"completed"=>$completed);
- // echo json_encode($return_arr);
 
  while ($row = mysqli_fetch_array($sel)) {
   $data[] = array("id"=>$row['id'],"task"=>$row['task'],"complete"=>$row['complete']);
@@ -67,36 +57,24 @@ if($request_type == 5){
  $Uid = $_POST['id'];
 
  mysqli_query($conn,"update toDo set complete = 0 where id = ('$Uid') ");
- // $lastinsert_id = mysqli_insert_id($conn);
- //
- // $return_arr[] = array("id"=>$lastinsert_id,"task"=>$task);
- // echo json_encode($return_arr);
 
  while ($row = mysqli_fetch_array($sel)) {
   $data[] = array("id"=>$row['id'],"task"=>$row['task'],"complete"=>$row['complete']);
  }
 }
 
-//not done
 // updating
 if($request_type == 6){
  $Uid = $_POST['id'];
  $task = $_POST['task'];
 
  mysqli_query($conn,"update toDo set task = '$task' WHERE id = ('$Uid') ");
- // $lastinsert_id = mysqli_insert_id($conn);
- //
- // $return_arr[] = array("id"=>$lastinsert_id,"task"=>$task);
- // echo json_encode($return_arr);
 
  while ($row = mysqli_fetch_array($sel)) {
   $data[] = array("id"=>$row['id'],"task"=>$row['task'],"complete"=>$row['complete']);
  }
 }
 
-
-
+//sends back data to html for the while row update pulls
  echo json_encode($data);
-
-
 ?>
